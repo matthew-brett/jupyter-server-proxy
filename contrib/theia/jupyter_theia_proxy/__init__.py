136a0b31aaea10e40f99f9b02f58a46ef7b325f7
Matthew Brett <matthew.brett@gmail.com>
2022-03-09 23:39:14 +0000

    Back to 0.0.0.0

--- contrib/theia/jupyter_theia_proxy/__init__.py
+++ contrib/theia/jupyter_theia_proxy/__init__.py
@@ -13,7 +13,7 @@ def setup_theia():
         full_path = shutil.which('theia')
         if not full_path:
             raise FileNotFoundError('Can not find theia executable in $PATH')
-        return ['theia', 'start', '.', '--hostname=127.0.0.1', '--port=' + str(port)]
+        return ['theia', 'start', '.', '--hostname=0.0.0.0', '--port=' + str(port)]
 
     return {
         'command': _theia_command,
